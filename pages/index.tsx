f1316fd8baaf6b310d6d1f0346df58489e4735ad
Drew Radcliff <sirampr@gmail.com>
2022-03-18 13:13:52 -0400

    Update styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import React, { ChangeEvent, KeyboardEvent, useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { v4 as uuidv4 } from "uuid";
+import { XIcon, MenuAlt4Icon } from "@heroicons/react/outline";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
@@ -10,12 +11,19 @@ const habitList = [{ id: uuidv4(), text: "🏃‍♀️", checked: false }];
 const Home: NextPage = () => {
   const [list, setList] = useState(habitList);
   const [text, setText] = useState("");
+  const [error, setError] = useState({});
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
   };
 
   const handleKeyDown = (e: KeyboardEvent) => {
+    if (text.length === 0) return;
+
+    if (text.length > 1) {
+      setError({ text: "Text can only be 1 character" });
+    }
+
     if (e.key === "Enter") {
       setList([...list, { id: uuidv4(), text: text, checked: false }]);
       setText("");
@@ -27,6 +35,10 @@ const Home: NextPage = () => {
       return item.id === id ? { ...item, checked } : item;
     });
     setList(newList);
+  };
+
+  const handleDelete = (id: string) => {
+    setList(list.filter((item) => item.id !== id));
   };
 
   return (
@@ -44,15 +56,24 @@ const Home: NextPage = () => {
             {list.map((item) => (
               <div
                 key={item.id}
-                className="flex items-center justify-between text-3xl"
+                className="flex items-center justify-between w-36 text-3xl hover:bg-gray-100 group rounded-md p-1"
               >
-                <p className="mr-4">{item.text}</p>
+                <div className="w-6 h-6">
+                  <MenuAlt4Icon className="group-hover:block hidden cursor-grab text-gray-500" />
+                </div>
+                <p className="w-[36px] text-center">{item.text}</p>
                 <input
                   className="w-6 h-6"
                   type="checkbox"
                   checked={item.checked}
                   onChange={() => handleCheck(item.id, !item.checked)}
                 />
+                <div className="w-6 h-6">
+                  <XIcon
+                    onClick={() => handleDelete(item.id)}
+                    className="group-hover:block hidden hover:cursor-pointer text-gray-500"
+                  />
+                </div>
               </div>
             ))}
           </div>
