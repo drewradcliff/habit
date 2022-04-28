333a23cb2f7d924ebc4ca48289bb1997e4d03f9c
drew <sirampr@gmail.com>
2022-04-28 00:46:06 -0400

    Add user relation to habit

--- pages/api/habits/index.ts
+++ pages/api/habits/index.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Habit } from "@prisma/client";
+import { getSession } from "next-auth/react";
 import { prisma } from "../../../db";
 
 export default async function handler(
@@ -7,24 +8,38 @@ export default async function handler(
   res: NextApiResponse<Habit[] | Habit>
 ) {
   const { method, body } = req;
+  const session = await getSession({ req });
 
-  switch (method) {
-    case "GET":
-      const habits = await prisma.habit.findMany();
-      res.status(200).json([...habits]);
-      break;
-    case "POST":
-      const { text, checked } = body;
-      const result = await prisma.habit.create({
-        data: {
-          text,
-          checked,
-        },
-      });
-      res.status(200).json(result);
-      break;
-    default:
-      res.setHeader("Allow", ["GET", "POST"]);
-      res.status(405).end(`Methdo ${method} Not Allowed`);
+  if (session) {
+    switch (method) {
+      case "GET":
+        const habits = await prisma.habit.findMany({
+          where: {
+            user: {
+              email: session?.user?.email,
+            },
+          },
+        });
+        res.status(200).json([...habits]);
+        break;
+      case "POST":
+        const { text, checked } = body;
+        const result = await prisma.habit.create({
+          data: {
+            text,
+            checked,
+            user: {
+              connect: {
+                email: session?.user?.email!,
+              },
+            },
+          },
+        });
+        res.status(200).json(result);
+        break;
+      default:
+        res.setHeader("Allow", ["GET", "POST"]);
+        res.status(405).end(`Methdo ${method} Not Allowed`);
+    }
   }
 }
