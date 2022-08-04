7704a23bbbd2e51a18049b1a875751a5d987c863
drew <sirampr@gmail.com>
2022-08-04 11:45:24 -0400

    Update favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -19,7 +19,10 @@ export default function Layout({
       <Head>
         <title>{title}</title>
         <meta name="description" content="habit tracker" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>☕️</text></svg>"
+        />
       </Head>
       <div className="container max-w-5xl min-h-screen flex flex-col justify-between mx-auto px-4">
         <div>{!hideNav && <Navbar />}</div>
